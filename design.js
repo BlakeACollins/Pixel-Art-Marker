37ec450474bda1f51695bd9a90b4dac4a30ae6e4
Blake Collins <bcollins3@comcast.net>
2019-04-26 20:39:03 -0700

    Add files via upload

--- design.js
+++ design.js
@@ -1,19 +1,45 @@
-$(document).ready(function() {
-    $('#sizePicker').submit(function makeGrid(grid){
-       const row_input = $('#input_height').val();
-       const col_input = $('#input_width').val();
-        for (var x = 1; x <= row_input; x++){
-            $('table').append('<tr></tr>');
-            for (var i = 1; i <= col_input; i++){
-                $('tr:last').append('<td></td>');
-                $('td').attr('Class', 'cells');
-        }
-        }
-        grid.preventDefault();
+function makeGrid() {
+	console.log("makeGrid is running!")
+	
+	// Select size input
+	
+	var canvas, cell, gridHeight, gridWidth, rows;
+	
+	canvas = $('#pixel_canvas');
+	gridHeight = $('#input_height').val();
+	gridWidth = $('#input_width').val();
+	
+	canvas.children().remove()
+	
+	for (x = 0; x < gridHeight; x++) {
+	canvas.append('<tr></tr>');
+	}
+	
+	rows = $('tr');
+	
+	for (y = 0; y < gridWidth; y++) {
+	rows.append('<td></td>');
+	} 
+	
+	cell = canvas.find('td');
+	
+	// When td is clicked by the user, change color of td
+	cell.click(function() {
+		// Select color input
+		console.log("changeColor is running!");
+		var color;
+		color = $("#colorPicker").val();
+		$(this).attr('bgcolor', color);
+	});
+	
+}
 
-        $('.cells').click(function(event){
-            var paint = $('#colorPicker').val();
-            $(event.target).css('background-color', paint);
-        });
-    });
+// When size is submitted by the user, call makeGrid()
+var submitQuery;
+
+submitQuery = $('input[type="submit"]')
+
+submitQuery.click(function(event) {
+  event.preventDefault();
+  makeGrid();
 });

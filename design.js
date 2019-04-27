253deb05f9fe5c94ecebafffd11fcebf396f0248
Blake Collins <bcollins3@comcast.net>
2019-04-26 20:08:37 -0700

    Add files via upload

---
+++ design.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#sizePicker').submit(function makeGrid(grid){
+        var row_input = $('#input_height').val();
+        var col_input = $('#input_width').val();
+        for (var x = 1; x <= row_input; x++){
+            $('table').append('<tr></tr>');
+            for (var i = 1; i <= col_input; i++){
+                $('tr:last').append('<td></td>');
+                $('td').attr('Class', 'cells');
+        }
+        }
+        grid.preventDefault();
+
+        $('.cells').click(function(event){
+            var paint = $('#colorPicker').val();
+            $(event.target).css('background-color', paint);
+        });
+    });
+});
